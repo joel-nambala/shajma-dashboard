3daefa738081c624ea1c46242c7864d9e08e4482
PROF. JOEL <joel.n.wanjala@gmail.com>
2023-05-30 16:17:51 +0300

    Updated the JSON data

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@
 
 // Select DOM Elements
 const sidebarLink = document.querySelectorAll('.sidebar-link');
+const recentOrdersBody = document.querySelector('.recent-orders-data');
+
+// State variables
+
+// prettier-ignore
+const codes = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h'];
 
 // Change active class on click
 sidebarLink.forEach(function (link) {
@@ -14,3 +20,72 @@ sidebarLink.forEach(function (link) {
     targetElement.closest('.sidebar-link').classList.add('active');
   });
 });
+
+// Generate random ids
+const generateRandomId = function () {
+  let result = '';
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  for (let i = 1; i <= 10; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
+console.log(generateRandomId());
+
+// String manipulation
+const stringUppercase = function (str) {
+  return str[0].toUpperCase() + str.slice(1);
+};
+
+// Number formatter
+const currencyFormatter = function (currency) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(currency);
+};
+
+// Get data from the json file
+const getRecentOrders = async function () {
+  try {
+    // Get the data from the json file
+    const response = await fetch('./data/data.json');
+
+    // Covert to a json string
+    const orders = await response.json();
+
+    // Loop over the data
+    orders.forEach(function (order, i, arr) {
+      // Generate a class
+      let domClass;
+
+      if (order.status === 'shipped') {
+        domClass = 'success';
+      } else if (order.status === 'pending') {
+        domClass = 'warning';
+      } else {
+        domClass = 'danger';
+      }
+
+      // Generate markup
+      const html = `
+        <tr>
+          <td>${order.name}</td>
+          <td>${order.code.toUpperCase()}</td>
+          <td>${currencyFormatter(order.price)}</td>
+          <td class="${domClass}">${stringUppercase(order.status)}</td>
+          <td>${stringUppercase(order.payment)}</td>
+        </tr>
+      `;
+
+      // Append to the UI
+      recentOrdersBody.insertAdjacentHTML('beforeend', html);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+getRecentOrders();
